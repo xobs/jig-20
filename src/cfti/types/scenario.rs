ffa00dc0fc977fba590e6e65fd7c589ef48af9f4
Sean Cross <sean@xobs.io>
2017-01-22 18:34:37 +0800

    scenario: convert Test list to string
    We'll need to do the loading in two stages.  First load the file and
    use strings, and later on resolve the strings to test files.
    
    Signed-off-by: Sean Cross <sean@xobs.io>

--- src/cfti/types/scenario.rs
+++ src/cfti/types/scenario.rs
@@ -7,8 +7,17 @@ pub struct Scenario {
     /// id: The string that other units refer to this file as.
     id: String,
 
+    /// name: Display name of this scenario.
+    name: String,
+
+    /// description: Paragraph describing this scenario.
+    description: Option<String>,
+
+    /// timeout: Maximum number of seconds this scenario should take.
+    timeout: u32,
+
     /// tests: A vector containing all the tests in this scenario.
-    tests: Vec<Test>,
+    tests: Vec<String>,
 
     /// exec_start: A command to run when starting tests.
     exec_start: Option<String>,
@@ -25,15 +34,29 @@ impl Scenario {
 
         // Load the .ini file
         let ini_file = match Ini::load_from_file(&path) {
-            Err(_) => return Err("Unable to load test file"),
+            Err(_) => return Err("Unable to load scenario file"),
             Ok(s) => s,
         };
 
         let scenario_section = match ini_file.section(Some("Scenario")) {
-            None => return Err("Test is missing '[Scenario]' section"),
+            None => return Err("Configuration is missing '[Scenario]' section"),
             Some(s) => s,
         };
 
+        let description = match scenario_section.get("Description") {
+            None => None,
+            Some(s) => Some(s.to_string()),
+        };
+
+        let name = match scenario_section.get("Name") {
+            None => path.to_string(),
+            Some(s) => s.to_string(),
+        };
+
+        let timeout = match scenario_section.get("Timeout") {
+            None => 2000,
+            Some(s) => s.parse().unwrap(),
+        };
 
         let exec_start = match scenario_section.get("ExecStart") {
             None => None,
@@ -56,10 +79,17 @@ impl Scenario {
             Some(s) => Some(s.to_string()),
         };
 
+        let tests = match scenario_section.get("Tests") {
+            None => return Err("Unable to find test list"),
+            Some(s) => s.split(|c| c == ',' || c == ' ').map(|s| s.to_string()).collect(),
+        };
 
         Ok(Scenario {
             id: id.to_string(),
-            tests: Vec::new(),
+            tests: tests,
+            timeout: timeout,
+            name: name,
+            description: description,
             exec_start: exec_start,
             exec_stop_success: exec_stop_success,
             exec_stop_failure: exec_stop_failure,
