ac77886d4162f22462abb43907a0c4eb2ca76e6c
Sean Cross <sean@xobs.io>
2017-01-22 18:19:42 +0800

    testset: support loading scenarios
    Load and support scenario objects.
    
    Signed-off-by: Sean Cross <sean@xobs.io>

--- src/cfti/testset.rs
+++ src/cfti/testset.rs
@@ -1,7 +1,7 @@
 use cfti::types::Test;
+use cfti::types::Scenario;
 /*
 use cfti::types::Jig;
-use cfti::types::Scenario;
 use cfti::types::Coupon;
 use cfti::types::Trigger;
 use cfti::types::Logger;
@@ -13,15 +13,16 @@ use cfti::types::Service;
 use std::collections::HashMap;
 use std::fs;
 use std::io::Error;
+use std::path::PathBuf;
 
 /// A `TestSet` object holds every known test in an unordered fashion.
 /// To run, a `TestSet` must be converted into a `TestTarget`.
 #[derive(Debug)]
 pub struct TestSet {
     tests: HashMap<String, Test>,
+    scenarios: HashMap<String, Scenario>,
     /*
     jigs: HashMap<String, Jig>,
-    scenarios: HashMap<String, Scenario>,
     coupons: HashMap<String, Coupon>,
     triggers: HashMap<String, Trigger>,
     loggers: HashMap<String, Logger>,
@@ -34,8 +35,10 @@ impl TestSet {
     /// Create a new `TestSet` from the given `dir`
     pub fn new(dir: &str) -> Result<TestSet, Error> {
 
-        let mut tests: HashMap<String, Test> = HashMap::new();
-//        let mut devs: HashMap<String, TestTarget> = HashMap::new();
+        let mut test_set = TestSet {
+            tests: HashMap::new(),
+            scenarios: HashMap::new(),
+        };
 
         for entry in try!(fs::read_dir(dir)) {
             let file = try!(entry);
@@ -45,26 +48,32 @@ impl TestSet {
                 continue;
             }
 
-            let new_test_name: String = path.file_stem().unwrap().to_str().unwrap().to_string();
+            test_set.add_item(path);
+        }
 
-            match path.extension() {
-                None => continue,
-                Some(entry) => if entry == "test" {
-                    let new_test = Test::new(path.to_str().unwrap().to_string()).unwrap();
-                    tests.insert(new_test_name.clone(), new_test);
+        Ok(test_set)
+    }
+
+    fn add_item(&mut self, path: PathBuf) {
+        let item_name = path.file_stem().unwrap().to_str().unwrap();
+        let path_str = path.to_str().unwrap();
+
+        match path.extension() {
+            None => return,
+            Some(entry) => {
+                if entry == "test" {
+                    let new_test = Test::new(item_name, path_str).unwrap();
+                    self.tests.insert(new_test.id().clone(), new_test);
+                } else if entry == "scenario" {
+                    let new_scenario = Scenario::new(item_name, path_str).unwrap();
+                    self.scenarios.insert(new_scenario.id().clone(), new_scenario);
                 }
                 else {
-                    println!("Unrecognized file type: {:?}", file);
-                    continue
+                    println!("Unrecognized file type: {:?}", path);
+                    return
                 }
             }
         }
-
-        let test_set = TestSet {
-            tests: tests,
-        };
-
-        Ok(test_set)
     }
 
     pub fn all_tests(&self) -> Vec<&Test> {
