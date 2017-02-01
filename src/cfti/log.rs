c7a6e7e570eb8d0fb483532e09992e79b4d7c513
Sean Cross <sean@xobs.io>
2017-02-01 14:24:20 +0800

    log: use "bus" instead of "chan"
    The "bus" package is one-to-many, which is exactly what we need.
    
    Signed-off-by: Sean Cross <sean@xobs.io>

--- src/cfti/log.rs
+++ src/cfti/log.rs
@@ -1,41 +1,85 @@
-extern crate chan;
+extern crate bus;
 use std::thread;
+use std::fmt;
+use std::time;
 
 #[derive(Debug)]
 pub enum LogError {
 
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
+pub struct LogItem {
+    /// A numerical indication of the type of message. 0 is internal messages such as test-start, 1 is test log output from various units, 2 is internal debug log.
+    message_type: u32,
+
+    /// The name of the unit that generated the message.
+    unit: String,
+
+    /// The type of unit, such as "test", "logger", "trigger", etc.
+    unit_type: String,
+
+    /// Number of seconds since the epoch
+    unix_time: u64,
+
+    /// Number of nanoseconds since the epoch
+    unix_time_nsecs: u32,
+
+    /// Textual representation of the message, minus linefeeds.
+    message: String,
+}
+
 pub struct Log {
-    log_tx_channel: chan::Sender<String>,
-    log_rx_channel: chan::Receiver<String>,
+    bus: bus::Bus<LogItem>,
+}
+
+impl fmt::Debug for Log {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Logger")
+    }
 }
 
 impl Log {
     pub fn new() -> Result<Log, LogError> {
 
-        // Create a new channel pair.  Log messages will be broadcast to "out"
-        let (tx_channel, rx_channel) = chan::async();
+        // Create a new channel pair.  Log messages will be broadcast to "rx"
+        let mut log: Log = Log {
+            bus: bus::Bus::new(4096),
+        };
 
-        let console_rx_channel = rx_channel.clone();
-        thread::spawn(move || {
-            loop {
-                let msg = match console_rx_channel.recv() {
-                    None => { println!("DEBUG!! Channel closed, probably quitting."); return; },
-                    Some(s) => s,
-                };
-                println!("DEBUG>>: {}", msg);
-            }
-        });
-
-        Ok(Log {
-            log_tx_channel: tx_channel,
-            log_rx_channel: rx_channel,
-        })
+        log.start_logger(|msg| println!("DEBUG>>: {:?}", msg));
+        Ok(log)
     }
 
-    pub fn debug(&self, msg: &str) {
-        self.log_tx_channel.send(msg.to_string());
+    pub fn start_logger<F>(&mut self, logger_func: F)
+        where F: Send + 'static + Fn(LogItem) {
+
+        let mut console_rx_channel = self.bus.add_rx();
+        thread::spawn(move ||
+            loop {
+                let msg = match console_rx_channel.recv() {
+                    Err(e) => { println!("DEBUG!! Channel closed, probably quitting.  Err: {:?}", e); return; },
+                    Ok(s) => s,
+                };
+                logger_func(msg);
+            }
+        );
+    }
+
+    pub fn debug(&mut self, msg: &str) {
+        let now = time::SystemTime::now();
+        let elapsed = match now.duration_since(time::UNIX_EPOCH) {
+            Ok(d) => d,
+            Err(e) => time::Duration::new(0, 0),
+        };
+
+        self.bus.broadcast(LogItem {
+            message_type: 2,
+            unit: "<internal>".to_string(),
+            unit_type: "<internal>".to_string(),
+            unix_time: elapsed.as_secs(),
+            unix_time_nsecs: elapsed.subsec_nanos(),
+            message: msg.to_string(),
+        });
     }
 }
