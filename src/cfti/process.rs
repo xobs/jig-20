e0272b69a14f660f2e2d0dce4e7dcf8ae8338d98
Sean Cross <sean@xobs.io>
2017-02-14 17:56:39 +0800

    process: make try_command_completion take FnOnce
    In order to be able to capture variables, indicate that the completion
    will be called only once.
    
    Signed-off-by: Sean Cross <sean@xobs.io>

--- src/cfti/process.rs
+++ src/cfti/process.rs
@@ -93,7 +93,7 @@ pub fn try_command(ts: &TestSet, cmd: &str, wd: &Option<String>, max: Duration) 
 
 pub fn try_command_completion<F>(cmd: &str, wd: &Option<String>, max: Duration, completion: F)
         -> Result<(ChildStdout, ChildStdin), CommandError>
-        where F: Send + 'static + Fn(Result<(), CommandError>)
+        where F: Send + 'static + FnOnce(Result<(), CommandError>)
 {
 
     let mut cmd = match make_command(cmd) {
