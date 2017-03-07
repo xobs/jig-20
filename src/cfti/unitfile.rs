472a0fd6acfb9d273d00e8c70bb5f6a2db68e0ca
Sean Cross <sean@xobs.io>
2017-03-07 15:05:47 +0800

    unitfile: parse files using systemd_parser
    This will, for example, allow us to have multiple ExecStart blocks.
    
    Signed-off-by: Sean Cross <sean@xobs.io>

--- src/cfti/unitfile.rs
+++ src/cfti/unitfile.rs
@@ -1,35 +1,62 @@
-extern crate ini;
-
-use self::ini::Ini;
+extern crate systemd_parser;
+use std::io::Read;
+use std::fs::File;
 
 pub enum UnitFileError {
-    FileLoadError,
+    FileUnreadable(String),
+    FileReadError(String),
+    FileParseError(String),
 }
 
 pub struct UnitFile {
-    ini_file: ini::Ini,
+    unitfile: systemd_parser::items::SystemdUnit,
 }
 
 impl UnitFile {
     pub fn new(path: &str) -> Result<UnitFile, UnitFileError> {
-        let ini_file = match Ini::load_from_file(path) {
-            Err(_) => return Err(UnitFileError::FileLoadError),
-            Ok(s) => s,
+
+        let mut contents = String::with_capacity(8192);
+         let mut file = match File::open(path) {
+            Ok(f) => f,
+            Err(e) => return Err(UnitFileError::FileUnreadable(format!("{:?}", e))),
+        };
+
+        if let Err(e) = file.read_to_string(&mut contents) {
+            return Err(UnitFileError::FileReadError(format!("{:?}", e)));
+        }
+
+        let unit_file = match systemd_parser::parse_string(&contents) {
+            Ok(u) => u,
+            Err(e) => return Err(UnitFileError::FileParseError(format!("{:?}", e))),
         };
 
         Ok(UnitFile {
-            ini_file: ini_file,
+            unitfile: unit_file,
         })
     }
 
     pub fn has_section(&self, name: &str) -> bool {
-        return self.ini_file.section(Some(name)).is_some()
+        self.unitfile.has_category(name)
     }
 
-    pub fn get(&self, section: &str, key: &str) -> Option<&String> {
-        match self.ini_file.section(Some(section)) {
-            None => None,
-            Some(sec) => sec.get(key),
+    pub fn get(&self, section: &str, key: &str) -> Option<&str> {
+        let coll = match self.unitfile.lookup_by_key(key) {
+            // Item not found at all.
+            None => return None,
+            Some(s) => s,
+        };
+
+        // If it's one or many, return the first item.
+        let ref coll = match coll {
+            &systemd_parser::items::DirectiveEntry::Solo(ref u) => u,
+            &systemd_parser::items::DirectiveEntry::Many(ref m) => &m[0],
+        };
+
+        // Item found in wrong section.
+        if coll.category() != section {
+            return None;
         }
+
+        coll.value()
     }
 }
